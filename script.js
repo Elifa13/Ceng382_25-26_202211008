e278e54bd8ad02bb837d6cdd9140559b02fee66a
Elifa13 <118261254+Elifa13@users.noreply.github.com>
2025-03-07 16:57:58 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function updateclock(){
+    let now = new Date();
+    let hours = now.getHours().toString().padStart(2, "0");
+    let minutes = now.getMinutes().toString().padStart(2,"0");
+    let seconds = now.getSeconds().toString().padStart(2,"0");
+
+    let timeString = hours+":"+minutes+":"+seconds;
+    document.getElementById("clock").innerText = timeString;
+
+    
+
+}
+setInterval(updateclock, 1000);
+updateclock();
